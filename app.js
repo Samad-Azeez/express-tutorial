e623642b202e901b0be1c27cd1dad4c65b60a8ed
Samad-Azeez <azeezsamad2004@gmail.com>
2024-08-11 23:00:37 +0100

    server update

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 import http from 'http';
 const server = http.createServer((req, res) => {
-    console.log('New connection:: server is running on port 3000....');
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('<h1>Hello, welcome to my server</h1>');
-    res.end();
+    if (url) {
+        
+    }
+    // console.log('New connection:: server is running on port 3000....');
+    // res.writeHead(200, { 'Content-Type': 'text/html' });
+    // res.write('<h1>Hello, welcome to my server</h1>');
+    // res.end();
 });
 
 server.listen(3000);
