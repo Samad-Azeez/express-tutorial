4ef0aaae06f36d9c5c09a0c557f3e4864c25b503
Samad-Azeez <azeezsamad2004@gmail.com>
2024-08-11 23:15:51 +0100

    server update

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 import http from 'http';
 const server = http.createServer((req, res) => {
-    if (url = '/'){
+    if (req.url === '/'){
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write('<h1>Hello, welcome to my server</h1>');
         res.end();
         
-    }else if (url = '/about'){
+    }else if (req.url === '/about'){
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write('<h1>About us</h1>');
         res.end();
