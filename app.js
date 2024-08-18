99708e67b5186989d89c849b551f79ed72083c6a
Samad-Azeez <azeezsamad2004@gmail.com>
2024-08-18 17:00:54 +0100

    feat: Add API routes for products and search functionality

--- app.js
+++ app.js
@@ -1,19 +1,57 @@
 import express from 'express';
 import path from 'path';
-import { fileURLToPath } from 'url';
+import { products } from "./data.js";
+// import { fileURLToPath } from 'url';
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+// const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 
-app.use(express.static('./navbar-app'));
+app.get('/', (req, res)=>{
+    res.send(`
+        <h1>Home page</h1>
+        <a href=./api/products>products</a>`);
+})
 
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './navbar-app/index.html'));
+app.get('/api/products', (req, res)=>{
+    const newProducts = products.map((products)=>{
+        const {id, name, image} = products;
+        return {id, name, image};
+    })
+    res.json(newProducts);
+})
+
+app.get('/api/products/:productID', (req, res) => {
+    const {productID} = req.params;
+    const singleProduct = products.find((product) => {
+        return product.id === Number(productID); // return the comparison result
+    });
+    if (!singleProduct) {
+        return res.status(404).send('product does not exist');
+    }
+    res.json(singleProduct); // Send the response after finding the product
 });
 
-app.all('*', (req, res) => {
-    res.send('<h1>resource not found</h1>');
-});
+app.get('/api/v1/query', (req, res)=>{
+    const {search, limit} = req.query;
+    let sortedProducts =[...products]
+
+    if (search) {
+        sortedProducts = sortedProducts.filter((product)=>{
+            return product.name.startsWith(search);
+        })
+    }
+
+    if (limit) {
+        sortedProducts = sortedProducts.slice(0, Number(limit));
+    }
+
+    if (sortedProducts.length < 1) {
+        // res.status(200).send('no products matched your search');
+        return res.status(200).json({success: true, data: []});
+    }
+
+    res.json(sortedProducts);
+})
 
 app.listen(3000, () => {
     console.log('Server is listening on port 3000...');
